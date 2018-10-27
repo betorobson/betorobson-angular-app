5b86319bc1f6f997ff9ea3190b63e9db03783d6f
Roberto Robson <betorobson@gmail.com>
2018-10-27 14:55:09 -0300

    hero component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../hero-mocks';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  test;
+  toggle = false;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  changeTest() {
+    this.toggle = !this.toggle;
+    this.test = this.toggle
+      ? 'x'
+      : 'b';
+  }
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.changeTest();
+  }
+
+}

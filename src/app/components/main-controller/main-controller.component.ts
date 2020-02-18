19439fe8f7db7fdd6e33a8f7d3bb34e32c01bcee
Roberto Robson <betorobson@gmail.com>
2020-02-18 15:43:06 -0300

    custom validators

--- src/app/components/main-controller/main-controller.component.ts
+++ src/app/components/main-controller/main-controller.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, Validators, FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
 import {FormlyFieldConfig} from '@ngx-formly/core';
 
 interface FormlyFieldConfig2 extends FormlyFieldConfig {
   blah?: string
+}
+
+function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    const forbidden = nameRe.test(control.value);
+    console.log(control.value);
+    return forbidden ? {'forbiddenName': {value: control.value}} : null;
+  };
 }
 
 @Component({
@@ -14,10 +22,23 @@ interface FormlyFieldConfig2 extends FormlyFieldConfig {
 
 export class MainControllerComponent {
 
-  form = new FormGroup({});
+  form = new FormGroup({
+    'name': new FormControl('', [
+      Validators.minLength(5),
+      forbiddenNameValidator(/bob/i)
+    ])
+  });
+
   model: any = {
     custom: 'blah'
   };
+
+  onSubmit (){
+    console.log(this.form.valid);
+
+    return;
+  };
+
   fields: FormlyFieldConfig2[] = [
     // {
     //   key: 'email',
@@ -33,6 +54,7 @@ export class MainControllerComponent {
       key: 'custom',
       type: 'custom',
       templateOptions: {
+        required: true,
         label: 'Custom inlined',
       }
     }
